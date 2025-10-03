f927763ef26e052aede9cbcd0631f986ffa4b28b
sababaradaran97 <sababaradaran97@gmail.com>
2025-10-03 14:10:18 +0330

    update 3

--- view/page_view.py
+++ view/page_view.py
@@ -1,67 +1,117 @@
 from tkinter import *
+from tkinter.messagebox import Message
+
+string = ""
 
 win = Tk()
 win.title( "calculator" )
 win.geometry( "300x390" )
 
 
-def entry ( master , text_name , text_width , text_x , text_y ) :
-    Entry( master , name = text_name , width = text_width , state = "readonly" ).place( x = text_x , y = text_y )
+def entry ( master , text_name , text_width , text_height , text_x , text_y ) :
+    Label( master , name = text_name , width = text_width , height = text_height , bg = "gray" ).place( x = text_x , y = text_y )
 
-name = "caculator"
-width = "42"
+name =""
+width = "36"
+height = "2"
 x = "25"
 y = "40"
 
-entry( win , name , width , x , y )
+entry( win , name , width , height , x , y )
+
 
 def show_result_9 () :
-    buttom_9.get()
+    global string
+    number = "9"
+    entry( win , number , width , height , x , y )
+    string = string + "9"
+
 
 def show_result_8 () :
-    buttom_8.get()
+    global string
+    number = "8"
+    entry( win , number , width , x , y )
+    string = string + number
 
 def show_result_7 () :
-    buttom_7.get()
+    global string
+    number = "9"
+    entry( win , "7" , width , x , y )
+    string = string + "7"
 
 def show_result_6 () :
-    buttom_6.get()
+    global string
+    number = "9"
+    entry( win , "6" , width , x , y )
+    string = string + "6"
 
 def show_result_5 () :
-    buttom_5.get()
+    global string
+    number = "9"
+    entry( win , "5" , width , x , y )
+    string = string + "5"
 
 def show_result_4 () :
-    buttom_4.get()
+    global string
+    number = "9"
+    entry( win , "4" , width , x , y )
+    string = string + "4"
 
 def show_result_3 () :
-    buttom_3.get()
+    global string
+    number = "9"
+    entry( win , "3" , width , x , y )
+    string = string + "3"
 
 def show_result_2 () :
-    buttom_2.get()
+    global string
+    number = "9"
+    entry( win , "2" , width , x , y )
+    string = string + "2"
 
 def show_result_1 () :
-    buttom_1.get()
+    global string
+    number = "9"
+    entry( win , "1" , width , x , y )
+    string = string + "1"
 
 def show_result_0 () :
-    buttom_0.get()
+    global string
+    number = "9"
+    entry( win , "0" , width , x , y )
+    string = string + "0"
 
 def show_result_minez () :
-    buttom_minez.get()
+    global string
+    number = "9"
+    string = string + "-"
 
 def show_result_plus () :
-    buttom_plus.get()
+    global string
+    number = "9"
+    string = string + "+"
 
 def show_result_equal () :
-    buttom_equal.get()
+    global string
+    string = string + "="
+    entry( win , eval(string) , width , x , y )
+    string = ""
 
 def show_result_devide () :
-    buttom_devide.get()
+    global string
+    string = string + "/"
 
 def show_result_multiplation () :
-    buttom_multipleation.get()
+    global string
+    string = string + "*"
 
 def show_result_dote () :
-    buttom_dote.get()
+    if "." in string :
+        Message.showerror( "rertete" , "reredufgehdj" )
+        
+    else :
+        entry( win , "." , name , width , x , y )    
+        string = string + "."
 
 
 
@@ -70,9 +120,9 @@ def show_result_dote () :
 #first line
 
 # buttom_9
-buttom_9 = StringVar
 buttom_9 = Button( win , text = "9" , command = show_result_9 )
 buttom_9.place( x = "25" , y = "100" , width = "50" , height = "40" )
+buttom_9.bind( "<Button>" , show_result_9 )
 
 # buttom_8
 buttom_8 = StringVar
