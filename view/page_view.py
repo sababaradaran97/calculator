4df69485ad57bc7c46a2d2c5eb13c06ed02b0db5
sababaradaran97 <sababaradaran97@gmail.com>
2025-10-04 17:55:13 +0330

    update 6 done

--- view/page_view.py
+++ view/page_view.py
@@ -54,83 +54,12 @@ for i in range( len( action ) ) :
 
     row = 2 - ( ( i - 1 ) // 3 )
     
-    button = Button( window, text = f"{ action[ i ] }", command = lambda i=i: show( i ) , width = 4 , height = 2 ).place( x = 140 , y = row * 50 + y_position )
+    button = Button( window, text = f"{ action[ i ] }", command = lambda i = i : show( i ) , width = 4 , height = 2 ).place( x = 140 , y = row * 50 + y_position )
 
 
 Button( window , text = "=" , command = finall , width = 4 , height = 2 ).place( x = 20 , y = 220 )
 
 Button( window , text = "." , command = dote , width = 4 , height = 2 ).place( x = 60 , y = 220 )
-
-
-
-
-
-
-"""
-for i in range( 10 ):
-
-    if i in [ 9 , 8 , 7 ] :
-
-        if i == 9 :
-            button = Button( window, text=f"{i}", command = lambda i=i: show( i ) , width = 4 , height = 2 ).place( x = 20 , y = 70 )
-
-        if i == 8 :
-            button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 70 , y = 70 )
-
-        if i == 7 :
-            button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 120 , y = 70 )
-
-    elif i in [ 6 , 5 , 4 ] :
-
-        if i == 6 :
-            button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 20 , y = 120 )
-
-        if i == 5 :
-            button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 70 , y = 120 )
-
-        if i == 4 :
-            button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 120 , y = 120 )
-        
-
-    elif i in [ 3 , 2 , 1 ] :
-        if i == 3 :
-            button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 20 , y = 170 )
-
-        if i == 2 :
-            button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 70 , y = 170 )
-
-        if i == 1 :
-            button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 120 , y = 170 )
-
-
-    else :
-        button = Button( window, text=f"{i}", command=lambda i=i: hello( i ) , width = 4 , height = 2 ).place( x = 20 , y = 220 )
-
-
-   
-for i in range( len( action ) ):
-
-    if action[ i ] == "+":
-        button = Button( window, text=f"{action[ i ]}", command=lambda act=action[ i ]: hello( act ) , width = 4 , height = 2 ).place( x = 170 , y = 70 )
-
-    if action[ i ] == "-":
-        button = Button( window, text=f"{action[ i ]}", command=lambda act=action[ i ]: hello( act ) , width = 4 , height = 2 ).place( x = 170 , y = 120 )
-
-    if action[ i ] == "/":
-        button = Button( window, text=f"{action[ i ]}", command=lambda act=action[ i ]: hello( act ) , width = 4 , height = 2 ).place( x = 170 , y = 170 )
-
-    if action[ i ] == "*":
-        button = Button( window, text=f"{action[ i ]}", command=lambda act=action[ i ]: hello( act ) , width = 4 , height = 2 ).place( x = 170 , y = 220 )
-
-    if action[ i ] == ".":
-        button = Button( window, text=f"{action[ i ]}", command=lambda act=action[ i ]: hello( act ) , width = 4 , height = 2 ).place( x = 70 , y = 220 )
-
-    if action[ i ] == "=":
-        button = Button( window, text=f"{action[ i ]}", command=lambda act=action[ i ]: hello( act ) , width = 4 , height = 2 ).place( x = 120 , y = 220 )
-
-    
-
-"""
 
 
 mainloop = window.mainloop()
